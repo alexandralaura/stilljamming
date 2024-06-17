bfac87b823f4421a7f1ae210df56a120d374de07
Alexandra Irwin <airwin357@gmail.com>
2024-06-18 08:59:53 +1000

    tues

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+
+  }
+};
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+    
+      <div className="App">
+         {/*add a searchbar component*/}
+        <div className="App-playlist">
+        {/* searchresults component*/}
+        {/* playlists component*/}
+
+         </div>
+      </div>
+   </div>
+    
+  );
+}
+
+export default App;
